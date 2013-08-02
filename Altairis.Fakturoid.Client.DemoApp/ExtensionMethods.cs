01850886de7318bf725fc68e6a4d2b7433da9892
Michal Altair Valasek <michal.valasek@altairis.cz>
2013-08-03 01:10:40 +0200

    Added demo application for API

---
+++ Altairis.Fakturoid.Client.DemoApp/ExtensionMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altairis.Fakturoid.Client.DemoApp {
+    internal static class ExtensionMethods {
+
+        public static void DumpProperties(this object element) {
+            element.DumpProperties(Console.Out);
+        }
+
+        public static void DumpProperties(this object element, TextWriter tw, string linePrefix = null) {
+            if (element == null) throw new ArgumentNullException("element");
+            if (tw == null) throw new ArgumentNullException("tw");
+
+            var propertyInfos = element.GetType().GetMembers(BindingFlags.Public | BindingFlags.Instance).OfType<PropertyInfo>();
+            foreach (var pi in propertyInfos) {
+                if (pi.PropertyType.IsValueType || pi.PropertyType == typeof(string)) {
+                    // Primitive type or string
+                    Console.WriteLine("{0}{1} = {2}", linePrefix, pi.Name, pi.GetValue(element));
+                }
+                else if (typeof(IEnumerable).IsAssignableFrom(pi.PropertyType)) {
+                    // Some kind of enumerable
+                    Console.WriteLine("{0}{1} = ... (enumerable)", linePrefix, pi.Name);
+                }
+                else {
+                    // Complex type
+                    Console.WriteLine("{0}{1}:", linePrefix, pi.Name);
+                    pi.GetValue(element).DumpProperties(tw, linePrefix + "  ");
+                }
+            }
+        }
+
+    }
+}
---
+++ Altairis.Fakturoid.Client.DemoApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Altairis.Fakturoid.Client;
+
+namespace Altairis.Fakturoid.Client.DemoApp {
+    class Program {
+        private static FakturoidContext context;
+
+        static void Main(string[] args) {
+            Console.WriteLine("Fakturoid API C#/.NET Client Demo Application");
+            Console.WriteLine("http://github.com/ridercz/Fakturoid-API");
+            Console.WriteLine("Copyright (c) Michal A. Valášek - Altairis, 2013");
+            Console.WriteLine();
+
+            // Verify commandline arguments 
+            if (args.Length != 2) {
+                Console.WriteLine("USAGE: fdemo accountname token");
+                return;
+            }
+            var accountName = args[0];
+            var accountToken = args[1];
+
+            // Create context
+            context = FakturoidContext.Create(accountName, accountToken, "Fakturoid API C#/.NET Client Demo Application (fakturoid@rider.cz)");
+            
+            // Do some magic
+            ShowAccountInfo();
+
+            // Wait for ENTER
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+        }
+
+        private static void ShowAccountInfo() {
+            Console.Write("Getting account information...");
+            var ai = context.Account.Select();
+            Console.WriteLine("OK");
+            ai.DumpProperties();
+            Console.WriteLine();
+        }
+
+    }
+}
