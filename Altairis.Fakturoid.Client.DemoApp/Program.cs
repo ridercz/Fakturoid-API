bab48875874b52277c69464eabcea6ba26c769f3
Michal Altair Valasek <michal.valasek@altairis.cz>
2013-08-03 08:35:42 +0200

    Added exception handling to demo app

--- Altairis.Fakturoid.Client.DemoApp/Program.cs
+++ Altairis.Fakturoid.Client.DemoApp/Program.cs
@@ -39,7 +39,7 @@ namespace Altairis.Fakturoid.Client.DemoApp {
 
         private static void ShowSubjects() {
             Console.Write("Creating new subject...");
-            var newSubject = new JsonSubject {
+            var subject = new JsonSubject {
                 name = "Altairis, s. r. o.",
                 street = "Bořivojova 35",
                 city = "Praha",
@@ -48,18 +48,37 @@ namespace Altairis.Fakturoid.Client.DemoApp {
                 registration_no = "27560911",
                 vat_no = "CZ27560911",
             };
-            var newId = context.Subjects.Create(newSubject);
+            var newId = context.Subjects.Create(subject);
             Console.WriteLine("OK, ID={0}", newId);
 
             Console.Write("Getting information about newly created subject...");
-            newSubject = context.Subjects.Select(newId);
+            subject = context.Subjects.Select(newId);
             Console.WriteLine("OK, listing properties:");
-            newSubject.DumpProperties(Console.Out, "\t");
+            subject.DumpProperties(Console.Out, "\t");
+
+            Console.Write("Updating subject...");
+            subject.custom_id = "MY_CUSTOM_ID";
+            subject = context.Subjects.Update(subject);
+            Console.WriteLine("OK, listing properties:");
+            subject.DumpProperties(Console.Out, "\t");
+
+            try {
+                Console.Write("Causing intentional error...");
+                context.Subjects.Create(subject);
+                Console.WriteLine("OK (that's unexpected!)");
+            }
+            catch (FakturoidException fex) {
+                Console.WriteLine("Failed! (that's expected)");
+                Console.WriteLine(fex.Message);
+                foreach (var item in fex.Errors) {
+                    Console.WriteLine("\tProperty '{0}': {1}", item.Key, item.Value);
+                }
+            }
 
             Console.Write("Deleting newly created contact...");
             context.Subjects.Delete(newId);
             Console.WriteLine("OK");
-
+                        
             Console.WriteLine();
         }
 
