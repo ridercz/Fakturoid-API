09d1403e3689b6d866d6fda876483cc5d31a810e
Michal Altair Valasek <michal.valasek@altairis.cz>
2013-08-04 10:33:00 +0200

    Renamed ExtensionMethods class to InternalExtensionMethods

---
+++ Altairis.Fakturoid.Client/InternalExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altairis.Fakturoid.Client {
+    internal static class InternalExtensionMethods {
+
+        public static void EnsureFakturoidSuccess(this HttpResponseMessage r) {
+            if (r == null) throw new ArgumentNullException("r");
+            if (r.IsSuccessStatusCode) return;
+
+            throw new FakturoidException(r);
+        }
+
+    }
+}
