df18208551609b6634798a8c289f38399ad638e7
Michal Altair Valasek <michal.valasek@altairis.cz>
2013-08-04 04:06:29 +0200

    InvoicingImport can now import invoices

--- Sandbox/InvoicingImport/Program.cs
+++ Sandbox/InvoicingImport/Program.cs
@@ -26,6 +26,60 @@ namespace InvoicingImport {
             // Process all operations
             PurgeAll();
             ImportContacts();
+            ImportInvoices();
+        }
+        
+        private static void ImportInvoices() {
+            Console.WriteLine("Importing invoices:");
+            using (var dc = new InvoicingEntities()) {
+                foreach (var i in dc.Invoices) {
+                    // Compute invoice number
+                    var invoiceNumber = string.Format("{0}01{1:00000}", i.DateTaxed.Year - 1980, i.SeqId);
+
+                    // Map old contact ID to new
+                    var subjectId = subjectIdMap[i.ContactId];
+                    Console.Write("  #{0}: {1}...", i.InvoiceId, invoiceNumber);
+
+                    // Create new invoice entity
+                    var newInvoice = new JsonInvoice {
+                        number = invoiceNumber,
+                        subject_id = subjectId,
+                        issued_on = i.DateCreated,
+                        taxable_fulfillment_due = i.DateTaxed,
+                        due_on = i.DateDue,
+                        lines = new List<JsonInvoiceLine>()
+                    };
+
+                    // Add invoice items
+                    foreach (var l in i.Items) {
+                        newInvoice.lines.Add(new JsonInvoiceLine {
+                            name = l.Name,
+                            quantity = l.Quantity,
+                            unit_name = l.Unit,
+                            unit_price = l.UnitPrice,
+                            vat_rate = l.Tax
+                        });
+                    }
+                    
+                    // Create invoice
+                    var newInvoiceId = context.Invoices.Create(newInvoice);
+                    Console.WriteLine("OK, ID={0}", newInvoiceId);
+
+                    // Get payment status
+                    if (i.Payments.Any()) {
+                        Console.Write("  #{0}: Updating payment status...", i.InvoiceId);
+                        context.Invoices.FireAction(newInvoiceId, InvoiceAction.Pay);
+                        //newInvoice = context.Invoices.SelectSingle(newInvoiceId);
+                        //newInvoice.paid_at = i.Payments.Max(x => x.DateReceived);
+                        //context.Invoices.Update(newInvoice);
+                    }
+                    else {
+                        Console.Write("  #{0}: Updating delivery status...", i.InvoiceId);
+                        context.Invoices.FireAction(newInvoiceId, InvoiceAction.MarkAsSent);
+                    }
+                    Console.WriteLine("OK");
+                }
+            }
         }
 
         private static void ImportContacts() {
@@ -59,10 +113,27 @@ namespace InvoicingImport {
                 }
             }
 
-            
+
         }
-        
+
         private static void PurgeAll() {
+            // Delete all invoices
+            Console.Write("Listing all invoices...");
+            var invoices = context.Invoices.Select(InvoiceType.All, InvoiceStatus.All);
+            Console.WriteLine("OK");
+
+            Console.WriteLine("Deleting invoices:");
+            foreach (var invoice in invoices) {
+                Console.Write("  #{0}: ({1}) {2}, {3:N2} Kč...", invoice.id, invoice.number, invoice.client_name, invoice.native_total);
+                try {
+                    context.Invoices.Delete(invoice.id);
+                    Console.WriteLine("OK");
+                }
+                catch (FakturoidException fex) {
+                    Console.WriteLine(fex.Message);
+                }
+            }
+
             // Delete all subjects
             Console.Write("Listing all subjects...");
             var subjects = context.Subjects.Select();
@@ -80,5 +151,6 @@ namespace InvoicingImport {
                 }
             }
         }
+
     }
 }
