8b904868058b2d37e926dba53112ae26fed549f9
Michal Altair Valášek <github@rider.cz>
2014-09-30 14:05:37 +0200

    Changed URL for APIv2

--- Altairis.Fakturoid.Client/FakturoidContext.cs
+++ Altairis.Fakturoid.Client/FakturoidContext.cs
@@ -12,41 +12,41 @@ namespace Altairis.Fakturoid.Client {
     /// Class representing connection to Fakturoid API, holds authentication information etc.
     /// </summary>
     public class FakturoidContext {
-        private const string DEFAULT_USER_AGENT = "C#/.NET API Client by Altairis (fakturoid@rider.cz)";
-        private const string API_BASE_URL_FORMAT = "https://{0}.fakturoid.cz/api/v1/";
+        private const string DEFAULT_USER_AGENT = "C#/.NET API Client v2 by Altairis (fakturoid@rider.cz)";
+        private const string API_BASE_URL_FORMAT = "https://app.fakturoid.cz/api/v2/accounts/{0}/";
 
         // Constructor
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FakturoidContext"/> class.
+        /// Initializes a new instance of the <see cref="FakturoidContext" /> class.
         /// </summary>
-        /// <param name="authenticationName">Name of the account used for authentication.</param>
+        /// <param name="accountName">Account name (accountName).</param>
+        /// <param name="emailAddress">The email address od user being authenticated.</param>
         /// <param name="authenticationToken">The authentication token.</param>
         /// <param name="userAgent">The User-Agent HTTP header value.</param>
-        /// <exception cref="System.ArgumentNullException">
-        /// authenticationName
+        /// <exception cref="System.ArgumentNullException">accountName
         /// or
         /// authenticationToken
         /// or
-        /// userAgent
-        /// </exception>
-        /// <exception cref="System.ArgumentException">
-        /// Value cannot be empty or whitespace only string.;authenticationName
+        /// userAgent</exception>
+        /// <exception cref="System.ArgumentException">Value cannot be empty or whitespace only string.;accountName
         /// or
         /// Value cannot be empty or whitespace only string.;authenticationToken
         /// or
-        /// Value cannot be empty or whitespace only string.;userAgent
-        /// </exception>
-        public FakturoidContext(string authenticationName, string authenticationToken, string userAgent = DEFAULT_USER_AGENT) {
-            if (authenticationName == null) throw new ArgumentNullException("authenticationName");
-            if (string.IsNullOrWhiteSpace(authenticationName)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "authenticationName");
+        /// Value cannot be empty or whitespace only string.;userAgent</exception>
+        public FakturoidContext(string accountName, string emailAddress, string authenticationToken, string userAgent = DEFAULT_USER_AGENT) {
+            if (accountName == null) throw new ArgumentNullException("accountName");
+            if (string.IsNullOrWhiteSpace(accountName)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "accountName");
+            if (emailAddress == null) throw new ArgumentNullException("emailAddress");
+            if (string.IsNullOrWhiteSpace(emailAddress)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "emailAddress");
             if (authenticationToken == null) throw new ArgumentNullException("authenticationToken");
             if (string.IsNullOrWhiteSpace(authenticationToken)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "authenticationToken");
             if (userAgent == null) throw new ArgumentNullException("userAgent");
             if (string.IsNullOrWhiteSpace(userAgent)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "userAgent");
 
             // Configuration properties
-            this.AuthenticationName = authenticationName;
+            this.AccountName = accountName;
+            this.EmailAddress = emailAddress;
             this.AuthenticationToken = authenticationToken;
             this.UserAgent = userAgent;
 
@@ -65,7 +65,15 @@ namespace Altairis.Fakturoid.Client {
         /// <value>
         /// The name of the Fakturoid account.
         /// </value>
-        public string AuthenticationName { get; private set; }
+        public string AccountName { get; private set; }
+
+        /// <summary>
+        /// Gets the email address.
+        /// </summary>
+        /// <value>
+        /// The email address.
+        /// </value>
+        public string EmailAddress { get; private set; }
 
         /// <summary>
         /// Gets the Fakturoid authentication token.
@@ -124,11 +132,11 @@ namespace Altairis.Fakturoid.Client {
         /// <returns>Instance of <see cref="System.Net.Http.HttpClient"/> class, initialized for use with Fakturoid API.</returns>
         internal HttpClient GetHttpClient() {
             // Get value of authentication header
-            var authHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes("X:" + this.AuthenticationToken));
+            var authHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Join(":", this.EmailAddress, this.AuthenticationToken));
 
             // Setup HTTP client
             var client = new HttpClient();
-            client.BaseAddress = new Uri(string.Format(API_BASE_URL_FORMAT, this.AuthenticationName));
+            client.BaseAddress = new Uri(string.Format(API_BASE_URL_FORMAT, this.AccountName));
             client.DefaultRequestHeaders.Add("User-Agent", this.UserAgent);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeader);
