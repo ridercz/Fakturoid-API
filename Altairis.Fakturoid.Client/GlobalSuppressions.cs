c1885c08daa450f919bd470bba7737ec0a10e3ca
Michal Altair Valasek <michal.valasek@altairis.cz>
2016-09-05 23:02:00 +0200

    Added code analysis exception rules.

---
+++ Altairis.Fakturoid.Client/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0072:'base.' is redundant and can safely be removed", Justification = "Legibility.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0069:Redundant explicit property name", Justification = "Consistency")]
+
