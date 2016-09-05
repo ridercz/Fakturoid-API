36ac6d2578818e7b7f5abf2c78ab4b1e69653d60
Michal Altair Valasek <michal.valasek@altairis.cz>
2016-09-05 23:16:46 +0200

    Bumped version to 2.5.0

--- Altairis.Fakturoid.Client.Purge/Properties/AssemblyInfo.cs
+++ Altairis.Fakturoid.Client.Purge/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Fakturoid API Demo Application")]
-[assembly: AssemblyCompany("Altairis, sb. r. o.")]
+[assembly: AssemblyCompany("Altairis, s. r. o.")]
 [assembly: AssemblyProduct("Fakturoid API Client")]
-[assembly: AssemblyCopyright("Copyright © Altairis, 2013-2014")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyCopyright("Copyright © Altairis, 2013-2016")]
+[assembly: AssemblyVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.5.0")]
--- Altairis.Fakturoid.Client/Properties/AssemblyInfo.cs
+++ Altairis.Fakturoid.Client/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 
 [assembly: AssemblyTitle("Fakturoid API v2 Client Library")]
 [assembly: AssemblyDescription("C#/.NET client library for fakturoid.cz API v2")]
-[assembly: AssemblyCompany("Altairis, sb. r. o.")]
+[assembly: AssemblyCompany("Altairis, s. r. o.")]
 [assembly: AssemblyProduct("Fakturoid API v2 Client")]
-[assembly: AssemblyCopyright("Copyright © Altairis, 2013-2015")]
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: AssemblyCopyright("Copyright © Altairis, 2013-2016")]
+[assembly: AssemblyVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.5.0")]
