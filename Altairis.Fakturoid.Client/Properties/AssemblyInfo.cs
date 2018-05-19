8f94cadce74a4eb63af30ec1f5c85d926f66c8f9
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-05-19 23:37:45 +0200

    Updated project to .NET Standard

--- Altairis.Fakturoid.Client/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Fakturoid API v2 Client Library")]
-[assembly: AssemblyDescription("C#/.NET client library for fakturoid.cz API v2")]
-[assembly: AssemblyCompany("Altairis, s. r. o.")]
-[assembly: AssemblyProduct("Fakturoid API v2 Client")]
-[assembly: AssemblyCopyright("Copyright © Altairis, 2013-2016")]
-[assembly: AssemblyVersion("2.5.0")]
-[assembly: AssemblyFileVersion("2.5.0")]
